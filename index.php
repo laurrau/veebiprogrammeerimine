40451ca454c56fea6a3da7f35c75b52bc1137afd
Lauri Raus <laurrau@users.noreply.github.com>
2018-09-11 14:22:23 +0300

    lisatud esimene fail

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset='utf-8'>
+	<title>Lauriii</title>
+</head>
+<body>
+<h1> Lauri Raus</h1>
+<p> Tere hommikust.Ava <a href="http://www.tlu.ee">TLÜ</a> veebileht</p>
+<p>See veebileht on testimise eesmärgil</p>
+<img src="../../~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_2.jpg" alt=">TLÜ Terra õppehoone">
+<p>Minu sõber teeb ka <a href="../../~krispol/" target="_blank">veebi</a></p>
+
+</body>
+</html>
